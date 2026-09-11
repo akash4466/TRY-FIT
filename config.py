8809231c03444b058cea66ef9287200ee1700c21
akash4466 <polakash918@gmail.com>
2026-09-11 13:01:17 +0530

    Fix server port configuration

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ DB_PORT = int(os.environ.get('DB_PORT', 3306))
 DB_USER = os.environ.get('DB_USER', 'root')
 DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
 DB_NAME = os.environ.get('DB_NAME', 'try-fit')
+
+# Web Server configurations
+PORT = int(os.environ.get('PORT', 8000))
+
+# OTP Delivery settings
